2b9fb94f014bd630746fdf2c55068f793712db79
David Markovic <psycho006business@gmail.com>
2023-06-27 21:19:28 +0200

    Update setup-music.js

--- commands/⚙️ Settings/setup-music.js
+++ commands/⚙️ Settings/setup-music.js
@@ -33,7 +33,7 @@ module.exports = {
       }))
       .setImage(message.guild.banner ? message.guild.bannerURL({
         size: 4096
-      }) : "https://imgur.com/jLvYdb4.png")
+      }) : "https://imgur.com/a/c4QQBb1")
       .setTitle(`Start Listening to Music, by connecting to a Voice Channel and sending either the **SONG LINK** or **SONG NAME** in this Channel!`)
       .setDescription(`> *I support <:Youtube:840260133686870036> Youtube, <:Spotify:846090652231663647> Spotify, <:soundcloud:825095625884434462> Soundcloud and direct MP3 Links!*`)
     ]
